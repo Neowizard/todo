9ed57a87ed07b4a6f32a7b99e9525154fbdfc92c
Neowizard <hk.neowizard@gmail.com>
2023-08-30 22:29:29 +0300

    Added support for editing and drag&dropping task items

--- static/js/script.js
+++ static/js/script.js
@@ -4,33 +4,121 @@ const taskList = document.getElementById('taskList');
 const deleteCheckedButton = document.getElementById('deleteChecked');
 const checkAllButton = document.getElementById('checkAll');
 
-
-// Load tasks from local storage
 let tasks = []
 
-// Render tasks
+
+function addDragDropEvents(li) {
+    li.draggable = true;
+    li.addEventListener('dragstart', (e) => {
+        const dragged = e.target.closest('li');
+        const dragged_idx = Array.from(taskList.children).indexOf(dragged)
+        e.dataTransfer.setData('text/plain', dragged_idx.toString());
+    });
+
+    li.addEventListener('dragenter', (e) => {
+        e.preventDefault();
+        const dragged = e.target.closest('li');
+        dragged.classList.add('drag-over');
+    });
+
+    li.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        const dragged = e.target.closest('li');
+        dragged.classList.add('drag-over');
+    });
+
+    li.addEventListener('dragleave', (e) => {
+        const dragged = e.target.closest('li');
+        dragged.classList.remove('drag-over');
+    });
+
+    li.addEventListener('drop', (e) => {
+        const dragged_idx = parseInt(e.dataTransfer.getData('text/plain'));
+        const drop = e.target.closest('li');
+        const drop_idx = Array.from(taskList.children).indexOf(drop);
+        drop.classList.remove('drag-over');
+
+        console.log(`Swapping ${dragged_idx} and ${drop_idx}`)
+        const new_tasks = arrayMove(tasks, dragged_idx, drop_idx);
+        tasks = new_tasks;
+        renderTasks();
+    });
+}
+
+function arrayMove(arr, from, to) {
+    let low;
+    let high;
+    if (from < to) {
+        low = from;
+        high = to;
+    } else {
+        low = to;
+        high = from;
+    }
+    const element_low = arr[low];
+    const element_high = arr[high];
+    const arr_start = arr.slice(0, low);
+    const arr_mid = arr.slice(low+1, high);
+    const arr_end = arr.slice(high+1);
+    return arr_start.concat(element_high).concat(arr_mid).concat(element_low).concat(arr_end);
+}
+
+function createTaskLi(tasks, taskIdx) {
+    const li = document.createElement('li');
+    const task = tasks[taskIdx];
+    li.id = `todo_${taskIdx}_li`
+    li.classList.add('taskLi');
+    li.innerHTML = `
+      <button class="deleteBtn" id="todo_${taskIdx}_delete">Delete</button>
+      <input type="checkbox" id="todo_${taskIdx}_check" ${task.completed ? 'checked' : ''}>
+      <span class="${task.completed ? 'completed' : ''}">${task.title}</span>
+      <button class="editBtn" id="todo_${taskIdx}_edit">&#x270e;</button>
+    `;
+    const checkbox = li.querySelector('input[type="checkbox"]');
+    const deleteBtn = li.querySelector('.deleteBtn');
+    const span = li.querySelector('span');
+    const editBtn = li.querySelector(`#todo_${taskIdx}_edit`);
+
+    checkbox.addEventListener('change', () => {
+        task.completed = checkbox.checked;
+        li.querySelector('span').classList.toggle('completed');
+    });
+
+    deleteBtn.addEventListener('click', () => {
+        tasks.splice(taskIdx, 1);
+        li.remove();
+    });
+
+    const editInput = document.createElement('input');
+    editInput.addEventListener('blur', () => {
+        span.textContent = editInput.value;
+        li.replaceChild(span, editInput);
+    });
+    editInput.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') {
+            span.textContent = editInput.value;
+            task.title = span.textContent;
+            li.replaceChild(span, editInput);
+            renderTasks();
+        }
+    });
+
+    editBtn.addEventListener('click', () => {
+        editInput.value = span.textContent;
+        li.replaceChild(editInput, span)
+
+    });
+
+    addDragDropEvents(li);
+
+    return li;
+}
+
 function renderTasks() {
     taskList.innerHTML = '';
+    taskList.draggable = true;
     for (let i = 0; i < tasks.length; i++) {
-        const task = tasks[i];
-        const li = document.createElement('li');
-        li.innerHTML = `
-      <button class="deleteBtn">Delete</button>
-      <input type="checkbox" ${task.completed ? 'checked' : ''}>
-      <span class="${task.completed ? 'completed' : ''}">${task.title}</span>
-    `;
-        const checkbox = li.querySelector('input[type="checkbox"]');
-        const deleteBtn = li.querySelector('.deleteBtn');
-
-        checkbox.addEventListener('change', () => {
-            task.completed = checkbox.checked;
-            li.querySelector('span').classList.toggle('completed');
-        });
-
-        deleteBtn.addEventListener('click', () => {
-            tasks.splice(i, 1);
-            li.remove();
-        });
+        const li = createTaskLi(tasks, i);
 
         taskList.appendChild(li);
     }
